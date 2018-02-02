b34bfab2943b609a3091fc204f89de2b9c21dda0
Alp Eren <erenalp@gmail.com>
2018-02-02 15:04:04 +0300

    session control

---
+++ kontrol.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($_POST) {
+
+  $Name=$_POST["Name"];
+  $Password=$_POST["Password"];
+
+
+  $dbhost = 'localhost';
+  $dbname = 'blog';
+  $dbuser = 'root';
+  $dbpass = '';
+
+  try {
+    $db = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",
+    $dbuser, $dbpass);
+  } catch (PDOException $e) {
+    echo 'Bağlantı kurulamadı';
+  }
+
+  $y= $db->prepare("select * from uyeler where uye_ad=? and uye_parola=?");
+  $y->execute(array($Name,$Password));
+
+  $a= $y->fetch(PDO::FETCH_ASSOC);
+  $b= $y->rowCount();
+
+  if ($b)
+  {
+    $_SESSION["admin"] = $a["uye_ad"];
+    echo "Hoşgeldin:  ";
+    echo $a["uye_ad"];
+    header("location:adminpanel.php");
+  }
+
+  else
+  {
+    echo "Hatalı Kullanıcı Adı veya Şifre Girdiniz...";
+  }
+
+}
+
+ ?>
